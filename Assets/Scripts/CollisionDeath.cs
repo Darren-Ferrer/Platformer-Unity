2d288b0b0458f5721a8221f31efb19c529ac2084
Darren Lee Ferrer <darrenferrer888@yahoo.com>
2020-04-07 19:40:13 -0700

    Working on death on collision

---
+++ Assets/Scripts/CollisionDeath.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionDeath : MonoBehaviour
+{
+    function OnTriggerEnter (collision : Collider)
+    {
+        print("Triggered: " + collision.tag);
+        if(collision.gameObject.tag == "Player")
+        {
+            Application.LoadLevel("Level 1"); //try integer value to verify
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
