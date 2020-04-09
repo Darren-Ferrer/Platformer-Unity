ef724ed0e9b423072084fa9721121f28721c5e3a
Darren Lee Ferrer <darrenferrer888@yahoo.com>
2020-04-08 23:41:49 -0700

    Added Animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
     public float runSpeed = 40f;
     float horizontalMove = 0f;
     bool jump = false;
@@ -24,9 +25,17 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump")) 
         {
             jump = true;
+            animator.SetBool("IsJumping",true);
         }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping",false);
     }
 }
